f4bd4ad04aff34b337032dd929025e7d24cbf2a3
Steffen Matheis <steffen.matheis.97@gmail.com>
2019-11-25 14:49:59 +0100

    add comment to redirectlogin middleware

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,3 +1,7 @@
+/**
+ * Redirect to login page if user not logged in (req.user not set)
+ * Do nothing if logged in
+ */
 const redirectLogin = (req, res, next) => {
   !req.user ? res.redirect('/login') : next();
 };
