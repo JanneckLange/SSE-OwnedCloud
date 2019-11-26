fb39c14162c87502515a8a4d078ca33703dc1a0b
lange <lange@infoboard.biz>
2019-11-26 12:41:32 +0100

    add test framework mocha/chai

---
+++ tests/shared/userData.js
@@ -0,0 +1,11 @@
+module.exports = {
+  user:{
+    username: 'Max Mustermann',
+    email: 'mustermann@mail.de',
+    password: '1234abc!',//8 Zeichen mit Buchstaben/Nummern/Sonderzeichen
+  },
+  wrong_data: {
+    email: ['hansPeter', 'hans@Peter', 'hansPeter.de'],
+    password: ['1234', 'test', 'unsafe!', '1234567890', 'LongUnsafePassword!']
+  }
+};
