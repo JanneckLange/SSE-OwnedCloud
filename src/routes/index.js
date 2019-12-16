832ef9fa9166fcf86e67d15f3f9dca1e864d803b
Bastian Böhm <contact.bastianboehm@gmail.com>
2019-12-16 22:42:17 +0100

    feat: page - display uploaded files for a user

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,4 +14,22 @@ router.get('/register', function(req, res, next) {
   res.render('register', {});
 });
 
+router.get('/files', function(req, res, next) {
+  res.render('user-files', {
+    user: {
+      name: 'Testuser',
+    },
+    files: [
+      {
+        id: '',
+        fileName: 'test-file.pdf',
+      },
+      {
+        id: '',
+        fileName: 'desktop.txt',
+      }
+    ]
+  });
+});
+
 module.exports = router;
